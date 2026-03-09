bad8ddfc2e5988f775ef67a8decefc895978faf9
sravanr788 <sravanr788@gmail.com>
2026-03-09 17:12:04 +0530

    feat(ui): review comments

--- dormsphere/src/app/app.routes.ts
+++ dormsphere/src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { HostelList } from './features/hostels/pages/hostel-list/hostel-list';
 import { HostelFormPage } from './features/hostels/pages/hostel-form-page/hostel-form-page';
 
 export const routes: Routes = [
-	{ path: '', pathMatch: 'full', redirectTo: 'hostels' },
-	{ path: 'hostels', component: HostelList },
-    { path: ':id/edit', component: HostelFormPage }
+  { path: '', pathMatch: 'full', redirectTo: 'hostels' },
+  { path: 'hostels', component: HostelList },
+  { path: 'hostels/:id/edit', component: HostelFormPage }
 ];
